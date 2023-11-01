e686a20a3f95447533feca3c667ae5ddca082479
Jaroslav Buday <jaroslav.buday@gmail.com>
2023-11-01 17:39:23 +0100

    priprava prepojenia a tvorba databazy skola v SQL

--- index.php
+++ index.php
@@ -1,36 +1,3 @@
-<?php
-$students = [
-        [
-            "first_name" => "Harry",
-            "second_name" => "Potter",
-            "age" => 11,
-            "life" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae temporibus ullam harum, nostrum nobis inventore, quaerat sit porro consequuntur, delectus eos.",
-            "college" => "Nebelvír",
-        ],
-        [
-            "first_name" => "Hermiona",
-            "second_name" => "Grangerová",
-            "age" => 12,
-            "life" => "Repudiandae temporibus ullam harum, nostrum nobis inventore, quaerat sit porro consequuntur, delectus eos. Ducimus facere doloribus fuga error? Consectetur dolorem praesentium recusandae!",
-            "college" => "Nebelvír",
-        ],
-        [
-            "first_name" => "Ron",
-            "second_name" => "Weasley",
-            "age" => 11,
-            "life" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae temporibus ullam harum, nostrum nobis inventore, quaerat sit porro consequuntur, delectus eos.",
-            "college" => "Nebelvír",
-        ],
-        [
-            "first_name" => "Draco",
-            "second_name" => "Malfoy",
-            "age" => 12,
-            "life" => "Amet consectetur adipisicing elit. Repudiandae temporibus ullam harum, nostrum nobis inventore, quaerat sit porro consequuntur, delectus eos. ",
-            "college" => "Zmijozel",
-        ],
-    ];
-?>
-
 <!DOCTYPE html>
 <html lang="sk-SK">
 <head>
@@ -44,10 +11,7 @@ $students = [
     </header>
     <main>
         <section>
-            <?php foreach($students as $one_student) : ?>
-                <h2><?php echo $one_student["first_name"]." ".$one_student["second_name"]  ?></h2>
-                
-            <?php endforeach  ?>
+            
             
         </section>
 
