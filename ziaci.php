7e564627b349075e1ee201f31c4d39647d6067df
Jaroslav Buday <jaroslav.buday@gmail.com>
2023-11-01 18:42:37 +0100

    Vypisanie dat z databazy na stranku

--- ziaci.php
+++ ziaci.php
@@ -13,21 +13,46 @@
             echo mysqli_connect_error();
             exit;
         }
-        echo "Úspešné prihlásenie do databázy";
+        // echo "Úspešné prihlásenie do databázy";
 
     //NASTAVENIE SQL DOTAZU
-        $sql = "SELECT FIRST_NAME FROM student";
-            echo "<br>";
+        $sql = "SELECT * FROM student";
+            // echo "<br>";
 
     // ODOSLANIE DOTAZU DO DATABAZY-VRATI OBJEKT
         $result = mysqli_query($connection, $sql); 
-            var_dump($result);
-            echo "<br>";
-            echo "<br>";
+            // var_dump($result);
+            // echo "<br>";
+            // echo "<br>";
 
     // PREHODIM SI OBJEKT NA ASOCIATIVNE POLE
         $students = mysqli_fetch_all($result, MYSQLI_ASSOC); 
-        var_dump($students);
+        // var_dump($students);
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Zoznam žiakov školy</h1>
+    <?php if(empty($students)):    ?>
+        <p>Žiadny žiaci v databáze</p>
+    <?php else:  ?>
+        <ul>
+            <?php foreach($students as $one_student): ?>
+                <li>
+                    <?php echo $one_student["first_name"]. " ".$one_student["second_name"] ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+
+    <?php endif; ?>
+
+</body>
+</html>
