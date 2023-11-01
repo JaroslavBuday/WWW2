ff2b9737048e32d91b71987ca888bafeb384b8c5
Jaroslav Buday <jaroslav.buday@gmail.com>
2023-11-01 18:14:36 +0100

    NACITANIE DAT Z databazy

--- ziaci.php
+++ ziaci.php
@@ -1,17 +1,33 @@
 <?php 
+    // NASTAVENIE - PRIHLASOVACIE UDAJE
+        $db_host = "localhost";  //pokial by nesiel localhost tak 127.0.0.1
+        $db_user = "test";
+        $db_password = "admin1234";
+        $db_name = "skola";
 
-    $db_host = "localhost";  //pokial by nesiel localhost tak 127.0.0.1
-    $db_user = "test";
-    $db_password = "admin1234";
-    $db_name = "skola";
+    // NASTAVENIE DATABAZOVEHO SPOJENIA
+        $connection = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 
-    $connection = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+    // KONTROLA CI NIE JE NEJAKA CHYBA
+        if(mysqli_connect_error()){
+            echo mysqli_connect_error();
+            exit;
+        }
+        echo "Úspešné prihlásenie do databázy";
 
-    if(mysqli_connect_error()){
-        echo mysqli_connect_error();
-        exit;
-    }
+    //NASTAVENIE SQL DOTAZU
+        $sql = "SELECT FIRST_NAME FROM student";
+            echo "<br>";
 
-    echo "Úspešné prihlásenie do databázy";
+    // ODOSLANIE DOTAZU DO DATABAZY-VRATI OBJEKT
+        $result = mysqli_query($connection, $sql); 
+            var_dump($result);
+            echo "<br>";
+            echo "<br>";
+
+    // PREHODIM SI OBJEKT NA ASOCIATIVNE POLE
+        $students = mysqli_fetch_all($result, MYSQLI_ASSOC); 
+        var_dump($students);
+
 
 ?>
