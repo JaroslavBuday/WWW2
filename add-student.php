9cf0606cc0450bc4b772e46acc9d9cf9b0e68a28
Jaroslav Buday <jaroslav.buday@gmail.com>
2023-11-03 11:14:02 +0100

    vytvorenie formuláru na pridanie ziaka iba frontend bez css

--- add-student.php
+++ add-student.php
@@ -7,6 +7,18 @@
 </head>
 <body>
     <?php require "assets/header.php"; ?>
+    <main>
+        <section class="add-form">
+            <form action="add-student.php" method="post">
+                <input type="text" placeholder="Meno" name="first_name" id=""><br>
+                <input type="text" placeholder="Priezvisko" name="second_name" id=""><br>
+                <input type="number" placeholder="Vek" min="6" name="age" id=""><br>
+                <textarea name="life" placeholder="Podrobnosti o žiakovi" ></textarea><br>
+                <input type="text" placeholder="Fakulta" name="college" id="">
+                <input type="submit" value="Pridať">
+            </form>
+        </section>
+    </main>
     <?php require "assets/footer.php"; ?>
 </body>
 </html>
