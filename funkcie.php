5926939416dcf5fda5d100cc7755472148bd4d0f
Jaroslav Buday <jaroslav.buday@gmail.com>
2023-11-05 13:09:32 +0100

    funkcie a return

--- funkcie.php
+++ funkcie.php
@@ -6,16 +6,17 @@
  * @param string $second_name - priezvisko študenta
  * @param integer $age - vek študenta
  * 
- * Vypíše popis študenta
+ * @return string popis študenta
  */
 
  
 // Vytvorenie funkcie - parametre
 function studentDescription($first_name, $second_name, $age){
-    echo "Toto je " . $first_name . " " . $second_name . " a je mu/jej " . $age . " rokov.";
-    echo "<br>";
-}
+    return "Toto je " . $first_name . " " . $second_name . " a je mu/jej " . $age . " rokov.<br>";
+    }
 
 // pouzitie funkcie - argumenty
-studentDescription("Harry", "Potter", 15);
-studentDescription("Hermiona", "Grangerova", 12);
+echo studentDescription("Harry", "Potter", 15);
+echo studentDescription("Hermiona", "Grangerova", 12);
+$student = studentDescription("Ron","Weasley",15) ;
+echo $student;
