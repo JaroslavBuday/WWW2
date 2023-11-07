a693339577bbf6ff11c3e92e78657f2b897c3c47
Jaroslav Buday <jaroslav.buday@gmail.com>
2023-11-07 09:35:52 +0100

    vytvorenie url.php a funkcia pre presmerovanie

---
+++ assets/url.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Presmerovanie na zadanu url adresu
+ * 
+ * @param string $path - adresa na ktoru sa ma presmerovat
+ * 
+ * @return void
+ */
+
+ function redirectUrl($path) {
+    if (isset($_SERVER["HTTPS"]) and $_SERVER["HTTPS"] != "off"){
+        $url_protocol = "https";
+    } else {
+        $url_protocol = "http";
+    }
+    
+    header("location: $url_protocol://" . $_SERVER["HTTP_HOST"] . $path); 
+}
+ 
