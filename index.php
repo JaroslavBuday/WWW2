447320b1e63db09e7c6aa684360bfaaff1128462
Jaroslav Buday <jaroslav.buday@gmail.com>
2023-11-10 11:25:41 +0100

    hlavicka a query  - video 142

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/general.css">
     <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./query/header-query.css">
     <script src="https://kit.fontawesome.com/81e746884d.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
