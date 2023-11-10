0fba2ab3404b1d02f83ac5f2725505404485830f
Jaroslav Buday <jaroslav.buday@gmail.com>
2023-11-10 10:58:07 +0100

    vytvorenie general.css

--- add-student.php
+++ add-student.php
@@ -31,6 +31,7 @@ if ($_SERVER["REQUEST_METHOD"]=== "POST"){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/general.css">
     <link rel="stylesheet" href="./css/header.css">
     <title>Document</title>
 </head>
--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/general.css">
     <link rel="stylesheet" href="./css/header.css">
     <script src="https://kit.fontawesome.com/81e746884d.js" crossorigin="anonymous"></script>
     <title>Document</title>
