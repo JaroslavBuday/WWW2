2a09a890b6f4edaa93c8a442a5d11dadc9a582a6
Jaroslav Buday <jaroslav.buday@gmail.com>
2023-12-01 12:12:04 +0100

    hashovanie hesla

--- admin/after-registration.php
+++ admin/after-registration.php
@@ -11,7 +11,14 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
     $first_name = $_POST["first-name"];
     $second_name = $_POST["second-name"];
     $email = $_POST["email"];
-    $password = $_POST["password"];
+    $password =  password_hash($_POST["password"], PASSWORD_DEFAULT);
 
-    var_dump($first_name, $second_name, $email, $password);
+    /* nikdy neukladat do databazy ako plain text
+            PASSWORD_DEFAULT - vzdy pouzije najnovsiu metodu */
+            
+    // Plain text = Admin123
+    // Hash = fggafg56a5f56ga5fg5a6sffga6f5g6a6dff5g
+
+    
+    var_dump($password);
 }
