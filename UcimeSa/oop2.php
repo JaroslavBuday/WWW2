9def2f427f1a6380f32fb11c6feae1e9094b4878
Jaroslav Buday <jaroslav.buday@gmail.com>
2023-12-14 09:12:38 +0100

    zapuzdrenie a protected, static

--- UcimeSa/oop2.php
+++ UcimeSa/oop2.php
@@ -3,9 +3,9 @@
 --   4 principy OOP --
 
 Zapuzdrenie (Encapsulation)
-    -private = atribut a metoda je pristupna iba vo vnutri class
+    -private = atribut a metoda je pristupna iba vo vnutri hlavnej class, nie je k dispozicii v class ktora dedi
     -public =  atribut a metoda je pristupna aj mimo class, nemusi sa vypisovat je to nastavene vzdy
-    -protected = 
+    -protected = atribut a metoda je pristupna v hlavnej class aj v class ktora dedi
 
 Abstrakcia (abstraction)
     -vytvori sa univerzalna class s najdolezitejsimi funkciami ktore sa daju pouzit pri roznych typoch dalsich class. Napr. vlastnosti uctu ktore sa pouzivaju pri roznych typoch uctov napr klasicky, podnikatelsky, sporiaci, ...
@@ -13,7 +13,7 @@ Abstrakcia (abstraction)
 Dedičnosť (Inheritance)
     - class berie metody z rodicovskej class. Napr univerzalne vlastnosti uctu do sporiaceho, podnikatelskeho , .. zapis= class podnikatelsky extends univerzal
 
-Polymorfizmus (Polimorphism)
+Polymorfizmus (Polimorphism) - obsah metody sa da podla potreby zmenit, alebo doplnit a je mozne pouzit nazov funkcie/metody ten isty - riadok 83 
 */
 
 // Logika
@@ -52,6 +52,10 @@ class bankAccount {
     private function add_movements($money){
         $this->movements[] = $money;
     }
+
+    static function description(){
+        echo "Na vašom účte je zostatok XY Eur.";
+    }
 }
 
 // Použitie
@@ -70,13 +74,27 @@ $account1->create_income(150);
 var_dump($account1->movements);
 echo "<br>";
 
+
+bankAccount::description(); // ked je nieco static da sa to rovno zavolat cez class::metoda()
+
+
+
 //    ----   DEDICNOST ----
 
 Echo "Dedicnost";echo "<br>";echo "<br>";
 
 class SaveAccount extends bankAccount {
-   
+   static function description(){
+        // echo "Na vašom sporiacom účte je zostatok XY Eur"; 
+            //polymorfizmus - funkcia description uz je v univerzalnom ucte jej obsah sa da ale podla potreby zmenit a je mozne pouzit nazov funkcie/metody ten isty vtedy prepise vlastne povodnu 
+        echo parent::description() . " Pekný deň prajeme!";
+            // takto zapisujeme keď chceme k pôvodnému textu ešte niečo pridať a nie ho prepísať
+    }
 }
 
 $account1 = new SaveAccount("Jozko","Varabel",4444);
 echo $account1->first_name;
+echo "<br>";
+$account1->description();
+
+
